6220402844e59039668497fad03dcf0f3e465107
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-23 22:21:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ function keepGoing(array, changeValue, skipValue) {
 }
 function findBy(array, findFn) {
   for(let i = 0; i < array.length; i++) {
-    
+     if (criteriaFunc(array[i])) {
+      return array[i];
+    }
   }
 }
